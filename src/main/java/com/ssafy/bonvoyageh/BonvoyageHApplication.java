713ffe6fd58cde26e8f96f8aedf5f2aeaa510ed9
jiwon <jiwonjeong37@gmail.com>
2023-05-02 11:09:51 +0900

    :tada: [Init] project start

---
+++ src/main/java/com/ssafy/bonvoyageh/BonvoyageHApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.bonvoyageh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonvoyageHApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonvoyageHApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/bonvoyageh/BonvoyageHApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.bonvoyageh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonvoyageHApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
