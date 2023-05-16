943f49572ce7694eaa99fdacad334ba54a36fd36
gofoxfox <gofoxfox@naver.com>
2023-05-16 17:06:18 +0900

    :rocket: [Feat] category(placeDetail) 관련 Dto 추가

---
+++ src/main/java/com/ssafy/bonvoyageh/model/place/category/ContentType28_ReportsDto.java
@@ -0,0 +1,20 @@
+package com.ssafy.bonvoyageh.model.place.category;
+
+public class ContentType28_ReportsDto {
+    String contentid;
+    String contenttypeid;
+    String openperiod;
+    String reservation;
+    String infocenterleports;
+    String scaleleports;
+    String accomcountleports;
+    String restdateleports;
+    String usetimeleports;
+    String usefeeleports;
+    String expagerangeleports;
+    String parkingleports;
+    String parkingfeeleports;
+    String chkbabycarriageleports;
+    String chkpetleports;
+    String chkcreditcardleports;
+}
---
+++ src/main/java/com/ssafy/bonvoyageh/model/place/category/ContentType38_ShoppingDto.java
@@ -0,0 +1,21 @@
+package com.ssafy.bonvoyageh.model.place.category;
+
+public class ContentType38_ShoppingDto {
+    String contentid;
+    String contenttypeid;
+    String saleitem;
+    String saleitemcost;
+    String fairday;
+    String opendateshopping;
+    String shopguide;
+    String culturecenter;
+    String restroom;
+    String infocentershopping;
+    String scaleshopping;
+    String restdateshopping;
+    String parkingshopping;
+    String chkbabycarriageshopping;
+    String chkpetshopping;
+    String chkcreditcardshopping;
+    String opentime;
+}
