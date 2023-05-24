d0d2497ecf2524d70c6b95ea29d978afd2043e3a
jiwon <jiwonjeong37@gmail.com>
2023-05-24 10:50:11 +0900

    :rocket: [Feat] timestamp -> date 변경

--- src/main/java/com/ssafy/bonvoyageh/model/review/ReviewDto.java
+++ src/main/java/com/ssafy/bonvoyageh/model/review/ReviewDto.java
@@ -2,14 +2,12 @@ package com.ssafy.bonvoyageh.model.review;
 
 import lombok.Data;
 
-import java.sql.Timestamp;
-
 @Data
 public class ReviewDto {
 
     int reviewId;
     int placeId;
-    Timestamp createTime;
+    String createTime;
     String content;
     float rate;
     String userId;
